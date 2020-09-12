8e61c40a20aef63424ca5ffa144b66bbbdc3fca2
Jefferson Frade <jefferrson.frade@gmail.com>
2020-09-12 01:38:51 -0300

    Refactored Console

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,13 +27,8 @@ $app = new Illuminate\Foundation\Application(
 */
 
 $app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
-);
-
-$app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
+    App\Core\Console\ConsoleKernel::class
 );
 
 $app->singleton(
